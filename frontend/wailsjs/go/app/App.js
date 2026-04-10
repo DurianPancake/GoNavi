33b113490f4d5b51ae837059b8709fba6865a8c2
DurianPankek <durianpankek@gmail.com>
2026-04-10 19:20:19 +0800

    🐛 fix(window): 修复 mac 原生全屏下输入时窗口丢失

--- frontend/wailsjs/go/app/App.js
+++ frontend/wailsjs/go/app/App.js
@@ -207,11 +207,15 @@ export function ImportLegacyGlobalProxy(arg1) {
 }
 
 export function InstallLocalDriverPackage(arg1, arg2, arg3, arg4) {
-    return window['go']['app']['App']['InstallLocalDriverPackage'](arg1, arg2, arg3, arg4);
+  return window['go']['app']['App']['InstallLocalDriverPackage'](arg1, arg2, arg3, arg4);
 }
 
 export function InstallUpdateAndRestart() {
   return window['go']['app']['App']['InstallUpdateAndRestart']();
+}
+
+export function LogWindowDiagnostic(arg1, arg2) {
+  return window['go']['app']['App']['LogWindowDiagnostic'](arg1, arg2);
 }
 
 export function MongoDiscoverMembers(arg1) {
